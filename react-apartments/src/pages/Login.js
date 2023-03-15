e8ceef993f52bd22f912749208455c9b0015e487
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-03-15 23:42:26 +0300

    add login request

--- react-apartments/src/pages/Login.js
+++ react-apartments/src/pages/Login.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
-function Login() {
-  return (
-    <div id="login">
-      <h2>Login</h2>
-    </div>
-  );
+import axios from 'axios';
+
+export default class Login extends React.Component {
+	state = {
+		user: ''
+	}
+
+	handleChange = event => {
+		this.setState({ name: event.target.value });
+	}
+
+	passChange = event => {
+		this.setState({ password: event.target.value});
+	}
+
+	handleSubmit = event => {
+		event.preventDefault();
+
+		const user = {
+			userName: this.state.name,
+			password: this.state.password
+		};
+
+		axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
+		.then(res => {
+			//console.log(res);
+			console.log(res.data);
+		})
+	}
+
+	render() {
+		return (
+			<div id="login">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">
+							<h2>Login</h2>
+						</div>
+						<div className="col-md-4 offset-md-4">
+							<form onSubmit={this.handleSubmit}>
+								<input type="text" onChange={this.handleChange}/>
+								<input type="password" onChange={this.passChange}/>
+								<button className="chief-btn" type="submit">Sign in</button>
+							</form>
+						</div>	
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
-
-
-export default Login;
