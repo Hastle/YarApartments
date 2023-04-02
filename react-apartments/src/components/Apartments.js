f9d88183d936cb5f44ff2800ff83b635146d20fe
Hastle <alexandrs.me.com@gmail.com>
2023-04-02 14:05:09 +0300

    add object array of apartments

--- react-apartments/src/components/Apartments.js
+++ react-apartments/src/components/Apartments.js
@@ -6,6 +6,38 @@ import apartment_4 from '../img/apartment-4.jpg';
 import apartment_5 from '../img/apartment-5.jpg';
 import apartment_6 from '../img/apartment-6.jpg';
 
+const Data = [
+	{
+		mainImage: apartment_1,
+		title: '2-комн. квартира, 90 м²',
+		address: 'Санкт-Петербург, р-н Центральный, Смольнинское, Невский просп., 136'
+	},
+	{
+		mainImage: apartment_2,
+		title: '1-комн. квартира, 61 м²',
+		address: 'Москва, ЗАО, р-н Дорогомилово, Кутузовский просп., 15'
+	},
+	{
+		mainImage: apartment_3,
+		title: '2-комн. квартира, 70 м²',
+		address: 'Ярославль, р-н Кировский, Республиканская ул., 13к2'
+	},
+	{
+		mainImage: apartment_4,
+		title: '3-комн. квартира, 88 м²',
+		address: 'Ярославль, р-н Фрунзенский, мкр. Суздалка, Московский просп., 129к2'
+	},
+	{
+		mainImage: apartment_5,
+		title: '2-комн. квартира, 70 м²',
+		address: 'Москва, ЗАО, р-н Дорогомилово, Кутузовский просп., 15'
+	},
+	{
+		mainImage: apartment_6,
+		title: '1-комн. квартира, 44 м²',
+		address: 'Краснодарский край, Сочи, р-н Адлерский, Урожайная ул., 71Ак3'
+	},
+]
 function Apartments() {
 	return (
 		<>
@@ -18,60 +50,19 @@ function Apartments() {
 						</div>
 					</div>
 					<div className="row">
+						{Data.map((Data =>
 						<div className="col-md-6 col-lg-4 mb-3">
 							<div className="apartment-card text-center">
-								<a className="thumbnail" href="#"><img src={apartment_1} alt=""/></a>
+								<a className="thumbnail" href="#">
+									<img src={Data.mainImage} alt=""/>
+								</a>
 								<div className="apartment-card-body">
-									<h4><a href="#">2-комн. квартира, 90 м²</a></h4>
-									<p>Санкт-Петербург, р-н Центральный, Смольнинское, Невский просп., 136</p>
+									<h4><a href="#">{Data.title}</a></h4>
+									<p>{Data.address}</p>
 								</div>
 							</div>
 						</div>
-						<div className="col-md-6 col-lg-4 mb-3">
-							<div className="apartment-card text-center">
-								<a className="thumbnail" href="#"><img src={apartment_2} alt=""/></a>
-								<div className="apartment-card-body">
-									<h4><a href="#">1-комн. квартира, 61 м²</a></h4>
-									<p>Москва, ЗАО, р-н Дорогомилово, Кутузовский просп., 15</p>
-								</div>
-							</div>
-						</div>
-						<div className="col-md-6 col-lg-4 mb-3">
-							<div className="apartment-card text-center">
-								<a className="thumbnail" href="#"><img src={apartment_3} alt=""/></a>
-								<div className="apartment-card-body">
-									<h4><a href="#">2-комн. квартира, 70 м²</a></h4>
-									<p>Ярославль, р-н Кировский, Республиканская ул., 13к2</p>
-								</div>
-							</div>
-						</div>
-						<div className="col-md-6 col-lg-4 mb-3">
-							<div className="apartment-card text-center">
-								<a className="thumbnail" href="#"><img src={apartment_4} alt=""/></a>
-								<div className="apartment-card-body">
-									<h4><a href="#">3-комн. квартира, 88 м²</a></h4>
-									<p>Ярославль, р-н Фрунзенский, мкр. Суздалка, Московский просп., 129к2</p>
-								</div>
-							</div>
-						</div>
-						<div className="col-md-6 col-lg-4 mb-3">
-							<div className="apartment-card text-center">
-								<a className="thumbnail" href="#"><img src={apartment_5} alt=""/></a>
-								<div className="apartment-card-body">
-									<h4><a href="#">2-комн. квартира, 70 м²</a></h4>
-									<p>Москва, ЗАО, р-н Дорогомилово, Кутузовский просп., 15</p>
-								</div>
-							</div>
-						</div>
-						<div className="col-md-6 col-lg-4 mb-3">
-							<div className="apartment-card text-center">
-								<a className="thumbnail" href="#"><img src={apartment_6} alt=""/></a>
-								<div className="apartment-card-body">
-									<h4><a href="#">1-комн. квартира, 44 м²</a></h4>
-									<p>Краснодарский край, Сочи, р-н Адлерский, Урожайная ул., 71Ак3</p>
-								</div>
-							</div>
-						</div>
+						))}
 					</div>
 				</div>
 			</section>
