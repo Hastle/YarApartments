a4d10bbff10ebc8794291b3a81836bbbbbe8f0b4
Hastle <alexandrs.me.com@gmail.com>
2023-04-02 14:25:32 +0300

    reident

--- react-apartments/src/components/Apartments.js
+++ react-apartments/src/components/Apartments.js
@@ -51,17 +51,17 @@ function Apartments() {
 					</div>
 					<div className="row">
 						{Data.map((Data =>
-						<div className="col-md-6 col-lg-4 mb-3">
-							<div className="apartment-card text-center">
-								<a className="thumbnail" href="#">
-									<img src={Data.mainImage} alt=""/>
-								</a>
-								<div className="apartment-card-body">
-									<h4><a href="#">{Data.title}</a></h4>
-									<p>{Data.address}</p>
+							<div className="col-md-6 col-lg-4 mb-3">
+								<div className="apartment-card text-center">
+									<a className="thumbnail" href="#">
+										<img src={Data.mainImage} alt=""/>
+									</a>
+									<div className="apartment-card-body">
+										<h4><a href="#">{Data.title}</a></h4>
+										<p>{Data.address}</p>
+									</div>
 								</div>
 							</div>
-						</div>
 						))}
 					</div>
 				</div>
