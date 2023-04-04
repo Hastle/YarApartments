6e42c778f934a6cf7c5196667fe75022e59e70e2
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-04-04 16:59:06 +0300

    add utils js to scrollToTop for react-router

--- react-apartments/src/pages/App.js
+++ react-apartments/src/pages/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import Login from '../pages/Login';
-import Apartments from '../pages/Apartments'
+import Apartments from '../pages/Apartments';
+
+import ScrollToTop from '../utils/ScrollToTop';
 
 function App() {
 	return (
 		<>
 			
 			<BrowserRouter>
+				<ScrollToTop />
 				<Routes>
 					<Route path="/" element={<MainPage />}/>
 					<Route path="/login" element={<Login />}/>
