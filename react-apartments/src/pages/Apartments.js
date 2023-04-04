98fe491667a1d531b10e9838ae1872d393c6e778
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-04-04 19:38:38 +0300

    add page apartments

--- react-apartments/src/pages/Apartments.js
+++ react-apartments/src/pages/Apartments.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import Data from '../Api/Data';
 import Wrapper from '../components/wrapper/Wrapper';
 
 function Apartments() {
 	return (
 		<Wrapper>
-			
+			<section id="apartments">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-6 text-center">
+							
+						</div>
+					</div>
+					<div className="row">
+						{Data.map((Data =>
+							<div className="col-md-6 col-lg-4 mb-3">
+								<div className="apartment-card text-center">
+									<a className="thumbnail" href="#">
+										<img src={Data.mainImage} alt=""/>
+									</a>
+									<div className="apartment-card-body">
+										<h4><a href="#">{Data.title}</a></h4>
+										<p>{Data.address}</p>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</section>
 		</Wrapper>
 	)
 }
