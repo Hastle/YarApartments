3432a53eb4de9624cdc94759eaf1d97095255c62
Hastle <alexandrs.me.com@gmail.com>
2023-04-07 17:26:46 +0300

    add utils scollToTop for scrolling top then use routes

--- react-apartments/src/pages/App.js
+++ react-apartments/src/pages/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
-import Login from '../pages/Login';
+import Login from './Login';
 import Apartments from '../pages/Apartments';
 
 import ScrollToTop from '../utils/ScrollToTop';
