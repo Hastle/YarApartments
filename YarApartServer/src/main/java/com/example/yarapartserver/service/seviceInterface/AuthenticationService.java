04fa89055eb6e9194e8fc8bf773e8b6e4ed2c834
nikolaivrzhesinsky <15nk33l49@gmail.com>
2023-04-09 12:43:21 +0300

    Revert "add email sender"
    This reverts commit 679384bc

---
+++ YarApartServer/src/main/java/com/example/yarapartserver/service/seviceInterface/AuthenticationService.java
@@ -0,0 +1,17 @@
+package com.example.yarapartserver.service.seviceInterface;
+
+import com.example.yarapartserver.dto.request.LogInDto;
+import com.example.yarapartserver.dto.request.RegistrationDto;
+import com.example.yarapartserver.dto.response.JwtResponse;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthenticationService {
+
+    boolean registration(RegistrationDto registrationDto);
+
+    JwtResponse authentication(LogInDto logInDto);
+
+    boolean activateUser(String code);
+
+}
