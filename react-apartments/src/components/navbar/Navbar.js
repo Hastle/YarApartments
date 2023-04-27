a289fda3d9cce24fdc30d63846784d769df480bb
Hastle <alexandrs.me.com@gmail.com>
2023-04-28 01:36:24 +0300

    add conditional render for login, profile links

--- react-apartments/src/components/navbar/Navbar.js
+++ react-apartments/src/components/navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import './navbar.sass';
+import AuthService from "../../services/auth.service";
 
 const activeLink = 'menu-item menu-item--active:after';
 const normalLink = 'menu-item'
 /*{({isActive}) => isActive ? activeLink : normalLink}*/
 function Navbar() {
+	const currentUser = AuthService.getCurrentUser();
 	return (
-		
 		<div className="top-navigation">
 			<div className="container">
 				<div className="main-menu">
@@ -17,7 +18,11 @@ function Navbar() {
 						<NavLink className="menu-item" to='/apartments'>Квартиры</NavLink>
 						<NavLink className="menu-item" to='/about'>О нас</NavLink>
 						<NavLink className="menu-item" to='/contacts'>Контакты</NavLink>
-						<Link className="primary-btn" to='/login'>Личный кабинет</Link>
+						{!currentUser ? (
+							<Link className="primary-btn" to='/login'>Личный кабинет</Link>
+						) : (
+							<Link className="menu-item" to='/profile'><i className="fa fa-user" aria-hidden="true"></i></Link>)
+						}
 					</nav>
 				</div>
 			</div>
