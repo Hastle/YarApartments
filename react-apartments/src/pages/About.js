e84ba0994df478d9053ed8ac9d3f125cc8764280
Hastle <alexandrs.me.com@gmail.com>
2023-05-01 14:44:40 +0300

    create pages About, Contacts

---
+++ react-apartments/src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Wrapper from '../components/wrapper/Wrapper';
+
+const About = () => {
+    return (
+        <Wrapper>
+            <section>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h3>О нас</h3>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </Wrapper>
+    );
+}
+
+export default About;
---
+++ react-apartments/src/pages/Contacts.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Wrapper from '../components/wrapper/Wrapper';
+
+const Contacts = () => {
+        return (
+            <Wrapper>
+                <section>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <h3>Контакты</h3>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </Wrapper>
+        );
+}
+
+export default Contacts;
