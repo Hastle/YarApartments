23bb5ed70e046c2dea98b0d7afa453a169b4c8dc
Hastle <alexandrs.me.com@gmail.com>
2023-05-05 12:26:45 +0300

    redirect from profile

--- react-apartments/src/pages/Profile.js
+++ react-apartments/src/pages/Profile.js
@@ -1,29 +1,34 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import {Navigate, useNavigate} from 'react-router-dom';
 import Wrapper from '../components/wrapper/Wrapper';
 import AuthService from "../services/auth.service";
 const Profile = () => {
-        const user = AuthService.getCurrentUser();
-        let navigate = useNavigate();
-        const logout = () => {
-            AuthService.logout();
-            navigate("/");
-        };
-        return (
-            <Wrapper>
+    const currentUser = AuthService.getCurrentUser();
+    let navigate = useNavigate();
+    const logout = () => {
+        AuthService.logout();
+        navigate("/");
+    };
+    return (
+        <Wrapper>
+            {currentUser ? (
                 <section>
                     <div className="container">
                         <div className="row">
                             <div className="col-md-12">
                                 <h3>Профиль</h3>
-                                <p>Имя пользователя: {user.userName}</p>
+                                <p>Имя пользователя: {currentUser.userName}</p>
                                 <button onClick={logout}>Выйти</button>
                             </div>
                         </div>
                     </div>
                 </section>
-            </Wrapper>
-        );
+            ) : (
+                <Navigate to="/" />
+                )
+            }
+        </Wrapper>
+    );
 }
 
 export default Profile;
