2e0a2d17a53a317881a881057a0d64fcc7326a27
Hastle <alexandrs.me.com@gmail.com>
2023-05-07 02:13:42 +0300

    create modal component

---
+++ react-apartments/src/components/modal/Modal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MdClose } from 'react-icons/md';
+import Styles from './modal.sass';
+const Modal = ({active, setActive, title, body, footer}) =>{
+    return (
+        <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
+            <div className="modal-content" onClick={e => e.stopPropagation()}>
+                <div className="modal-header">
+                    <button className="close-btn" onClick={() => setActive(false)}><MdClose/></button>
+                    <h4>{title}</h4>
+                </div>
+                <div className="modal-body">
+                    {body}
+                </div>
+                <div className="modal-footer">
+                    {footer}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
