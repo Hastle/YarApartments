5c67249f8f48cd89b304a83307dab25902da39e2
Hastle <alexandrs.me.com@gmail.com>
2023-05-07 02:23:10 +0300

    add and test modal component

--- react-apartments/src/pages/Contacts.js
+++ react-apartments/src/pages/Contacts.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Wrapper from '../components/wrapper/Wrapper';
+import Modal from "../components/modal/Modal";
 
 const Contacts = () => {
-        return (
-            <Wrapper>
-                <section>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-12">
-                                <h3>Контакты</h3>
-                            </div>
+    const [modalActive, setModalActive] = useState(false);
+    const bodyContent = (
+        <div className="text-center">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit numquam provident sit tenetur totam? A ab aperiam cupiditate debitis, dignissimos ex incidunt iure labore nemo nisi quas quisquam velit veritatis?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit numquam provident sit tenetur totam? A ab aperiam cupiditate debitis, dignissimos ex incidunt iure labore nemo nisi quas quisquam velit veritatis?</p>
+        </div>
+    )
+    const footerContent = (
+        <button className="primary-btn">Читать</button>
+    )
+    return (
+        <Wrapper>
+            <section>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h3>Контакты</h3>
+                            <button className="primary-btn" onClick={() => setModalActive(true)}>Модалка</button>
+                            <Modal active={modalActive} setActive={setModalActive} title="Lorem" body={bodyContent} footer={footerContent}/>
                         </div>
                     </div>
-                </section>
-            </Wrapper>
-        );
+                </div>
+            </section>
+        </Wrapper>
+    );
 }
 
 export default Contacts;
