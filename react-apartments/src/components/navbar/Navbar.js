1881e6912f16cf50ad54fb4110dfc416a5747acb
Hastle <alexandrs.me.com@gmail.com>
2023-05-10 23:27:14 +0300

    create dropdown menu

--- react-apartments/src/components/navbar/Navbar.js
+++ react-apartments/src/components/navbar/Navbar.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import './navbar.sass';
-import AuthService from "../../services/auth.service";
+import AuthService from '../../services/auth.service';
+import useOutsideAlerter from "../../utils/useOutsideAlerter";
+import {FiMenu} from 'react-icons/fi';
+import {FaUser} from 'react-icons/fa';
 
-const activeLink = 'menu-item menu-item--active:after';
-const normalLink = 'menu-item'
 /*{({isActive}) => isActive ? activeLink : normalLink}*/
 function Navbar() {
 	const currentUser = AuthService.getCurrentUser();
+	const {ref, isOpen, setIsOpen} = useOutsideAlerter(false);
+	const toggleOpen = () => {
+		isOpen === true ? setIsOpen(false) : setIsOpen(true);
+	}
 	return (
 		<div className="top-navigation">
 			<div className="container">
 				<div className="main-menu">
 					<NavLink className="logo" to='/'>YarApartments.</NavLink>
-					<nav className="sub-menu">
+					<div className="sub-menu">
+						<button className="menu-btn" onClick={toggleOpen}>
+							<FiMenu size="24px" />
+						</button>
+					</div>
+					<div className={isOpen ? "dropdown-menu active" : "dropdown-menu"} onClick={toggleOpen} ref={ref}>
 						<NavLink className="menu-item" to='/'>Главная</NavLink>
 						<NavLink className="menu-item" to='/apartments'>Квартиры</NavLink>
 						<NavLink className="menu-item" to='/about'>О нас</NavLink>
 						<NavLink className="menu-item" to='/contacts'>Контакты</NavLink>
+						<hr/>
 						{!currentUser ? (
-							<Link className="primary-btn" to='/login'>Личный кабинет</Link>
+							<>
+								<Link className="menu-item" to='/login'>Зарегистрироваться</Link>
+								<Link className="menu-item" to='/login'>Войти</Link>
+							</>
 						) : (
-							<Link className="menu-item" to='/profile'><i className="fa fa-user" aria-hidden="true"></i></Link>)
+							<Link className="menu-item" to='/profile'><FaUser /> Профиль</Link>)
 						}
-					</nav>
+					</div>
 				</div>
 			</div>
 		</div>
