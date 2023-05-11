6a002a9e3d5f448999cea874ef225dc08de9146c
Hastle <alexandrs.me.com@gmail.com>
2023-05-12 02:00:44 +0300

    add logout button, use grid for dropdown-menu

--- react-apartments/src/components/navbar/Navbar.js
+++ react-apartments/src/components/navbar/Navbar.js
@@ -1,14 +1,19 @@
 import React, {useState} from 'react';
-import {Link, NavLink} from 'react-router-dom';
+import {Link, NavLink, Navigate, useNavigate} from 'react-router-dom';
 import './navbar.sass';
 import AuthService from '../../services/auth.service';
 import useOutsideAlerter from "../../utils/useOutsideAlerter";
 import {FiMenu} from 'react-icons/fi';
-import {FaUser} from 'react-icons/fa';
+//import {FaUser} from 'react-icons/fa';
 
 /*{({isActive}) => isActive ? activeLink : normalLink}*/
 function Navbar() {
 	const currentUser = AuthService.getCurrentUser();
+	let navigate = useNavigate();
+	const logout = () => {
+		AuthService.logout();
+		navigate("/");
+	};
 	const {ref, isOpen, setIsOpen} = useOutsideAlerter(false);
 	const toggleOpen = () => {
 		isOpen === true ? setIsOpen(false) : setIsOpen(true);
@@ -24,7 +29,7 @@ function Navbar() {
 							<FiMenu size="24px" />
 						</button>
 					</div>
-					<div className={isOpen ? "dropdown-menu active" : "dropdown-menu"}>
+					<div className={isOpen ? "dropdown-menu active col-sm-12 col-md-3 no-gutter" : "dropdown-menu"}>
 						<NavLink className="menu-item" to='/'>Главная</NavLink>
 						<NavLink className="menu-item" to='/apartments'>Квартиры</NavLink>
 						<NavLink className="menu-item" to='/about'>О нас</NavLink>
@@ -36,8 +41,11 @@ function Navbar() {
 								<Link className="menu-item" to='/login'>Войти</Link>
 							</>
 						) : (
-							<Link className="menu-item" to='/profile'><FaUser /> Профиль</Link>)
-						}
+							<>
+								<Link className="menu-item" to='/profile'>Профиль</Link>
+								<a className="menu-item text--warning" onClick={logout}>Выйти</a>
+							</>
+						)}
 					</div>
 				</div>
 			</div>
