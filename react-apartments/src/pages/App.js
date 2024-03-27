b3a1c10c98aae96114989d6d66ca20c85e4de0a4
Hastle <alexandrs.me.com@gmail.com>
2024-03-27 04:14:30 +0300

    wrapped App with Layout component

--- react-apartments/src/pages/App.js
+++ react-apartments/src/pages/App.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import Apartments from '../pages/Apartments';
 import About from '../pages/About';
 import Contacts from "./Contacts";
 import Login from './Login';
 import Profile from '../pages/Profile';
+import Layout from '../components/Layout/Layout';
 
 import ScrollToTop from '../utils/ScrollToTop';
 
-
 function App() {
 	return (
-		<>
-			
-			<BrowserRouter>
-				<ScrollToTop />
-				<Routes>
+		<BrowserRouter>
+			<ScrollToTop />
+			<Routes>
+				<Route element={<Layout />}>
 					<Route path="/" element={<MainPage />}/>
 					<Route path="/apartments" element={<Apartments />}/>
 					<Route path="/about" element={<About />}/>
 					<Route path="/contacts" element={<Contacts />}/>
 					<Route path="/login" element={<Login />}/>
 					<Route path="/profile" element={<Profile />}/>
-				</Routes>
-			</BrowserRouter>
-
-		</>
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
