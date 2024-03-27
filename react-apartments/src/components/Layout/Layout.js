f8bcfa51f9359df11bf4375f4e0f64b27cd429ad
Hastle <alexandrs.me.com@gmail.com>
2024-03-27 04:18:21 +0300

    remove unused props

--- react-apartments/src/components/Layout/Layout.js
+++ react-apartments/src/components/Layout/Layout.js
@@ -3,7 +3,7 @@ import {Outlet} from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-function Layout(props) {
+function Layout() {
 	return (
 		<>
 			<Header />
