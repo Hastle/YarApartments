c055854ac43a340a44895f1dd2a1248fdd884590
Hastle <alexandrs.me.com@gmail.com>
2024-04-02 04:05:05 +0300

    create Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaCheck } from "react-icons/fa";
+import { FaRubleSign } from "react-icons/fa";
+import grid from "../../styles/grid.module.sass";
+import styles from "./styles.module.sass";
+
+function Card({ address, price, imageUrl }) {
+	return (
+		<Link to="/{id}">
+			<div className={styles.card}>
+				<img
+					src={imageUrl}
+					alt={address}
+					className={styles.cardImage}
+				/>
+				<div className={styles.cardContent}>
+					<div className={`${grid["mb-3"]} ${styles.cardPrice}`}>
+						{price} <FaRubleSign />
+					</div>
+					<div className={`${styles.cardAddress}`}>{address}</div>
+				</div>
+			</div>
+		</Link>
+	);
+}
+
+export default Card;
