d4c20ff27f3fac6e272859892fdea868ba144198
Hastle <alexandrs.me.com@gmail.com>
2024-04-02 04:03:07 +0300

    create fake data

---
+++ src/data/DataApartments.js
@@ -0,0 +1,53 @@
+import apartment_1 from "../assets/images/apartment-1.jpg";
+import apartment_2 from "../assets/images/apartment-2.jpg";
+import apartment_3 from "../assets/images/apartment-3.jpg";
+import apartment_4 from "../assets/images/apartment-4.jpg";
+import apartment_5 from "../assets/images/apartment-5.jpg";
+import apartment_6 from "../assets/images/apartment-6.jpg";
+
+const data = [
+	{
+		id: "0",
+		title: "Apartment 1",
+		price: "27 992",
+		address: "Ярославль, р-н Кировский, Республиканская ул., 13к2",
+		imageUrl: apartment_1,
+	},
+	{
+		id: "1",
+		title: "Apartment 2",
+		price: "35 600",
+		address: "Москва, ул. Ленина, 24",
+		imageUrl: apartment_2,
+	},
+	{
+		id: "2",
+		title: "Apartment 3",
+		price: "40 200",
+		address: "Санкт-Петербург, пр. Невский, 22",
+		imageUrl: apartment_3,
+	},
+	{
+		id: "3",
+		title: "Apartment 4",
+		price: "29 800",
+		address: "Екатеринбург, ул. Мира, 15",
+		imageUrl: apartment_4,
+	},
+	{
+		id: "4",
+		title: "Apartment 5",
+		price: "33 500",
+		address: "Новосибирск, ул. Ленинградская, 8",
+		imageUrl: apartment_5,
+	},
+	{
+		id: "5",
+		title: "Apartment 6",
+		price: "31 700",
+		address: "Красноярск, ул. Пушкина, 10",
+		imageUrl: apartment_6,
+	},
+];
+
+export default data;
