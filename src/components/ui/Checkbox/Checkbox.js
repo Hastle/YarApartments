690f42b299c568e4d8cbc30cef92e45493013f19
Hastle <alexandrs.me.com@gmail.com>
2024-04-02 04:11:00 +0300

    add disabled props

--- src/components/ui/Checkbox/Checkbox.js
+++ src/components/ui/Checkbox/Checkbox.js
@@ -2,12 +2,15 @@ import React from "react";
 import styles from "./styles.module.sass";
 import { FaCheck } from "react-icons/fa";
 
-function Checkbox({ label, checked, onChange }) {
+function Checkbox({ label, checked, disabled, onChange }) {
 	return (
-		<label className={styles.checkbox_container}>
+		<label
+			className={`${styles.checkbox_container} ${disabled ? styles.disabled : ""}`}
+		>
 			<input
 				type="checkbox"
 				checked={checked}
+				disabled={disabled}
 				onChange={onChange}
 				className={styles.checkbox_input}
 			/>
