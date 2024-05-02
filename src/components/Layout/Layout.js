d2fed5366f3b4e99bf1867ed4fbe460fa37a635a
Hastle <alexandrs.me.com@gmail.com>
2024-05-03 02:51:16 +0300

    use grid classes for Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import grid from "../../styles/grid.module.sass";
 
 function Layout() {
 	return (
 		<>
 			<Header />
-			<Outlet />
+			<div className={grid["container"]}>
+				<div className={grid["row"]}>
+					<Outlet />
+				</div>
+			</div>
 			<Footer />
 		</>
 	);
