8aefeea3b86c836e42fffbdfe8af08dd5ff3e80e
Hastle <alexandrs.me.com@gmail.com>
2024-05-05 05:47:22 +0300

    use initial global.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
-import "./styles/main.sass";
+import "./styles/global.css";
+// import "./styles/global.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
