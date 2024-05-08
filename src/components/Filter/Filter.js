0b08aec20a910268d0c7697bf996da893bc4aa8e
Hastle <alexandrs.me.com@gmail.com>
2024-05-08 04:54:11 +0300

    test react-hook-form hook

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,37 +1,97 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import Input from "../ui/Input/Input";
 import Checkbox from "../ui/Checkbox/Checkbox";
-import Switch from "../ui/Switch/Switch";
 import Button from "../ui/Button/Button";
 
 function Filter() {
+	const { register, handleSubmit, reset } = useForm();
+
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
+	const handleReset = () => {
+		reset({
+			priceFrom: "",
+			priceTo: "",
+			rooms: {
+				1: false,
+				2: false,
+				3: false,
+				4: false,
+			},
+			additional: {
+				noDeposit: false,
+				noPrepayment: false,
+				balcony: false,
+				pets: false,
+			},
+		});
+	};
+
 	return (
-		<>
-			<h3 className="text-xl">Цена</h3>
-			<div className="flex flex-wrap">
-				<Input type="text" placeholder="" required={true} />
-				<Input type="text" placeholder="" required={true} />
+		<div className="p-3">
+			<div className="flex justify-between">
+				<div className="font-bold text-base">Фильтры</div>
+				<button
+					className="font-bold text-base"
+					type="reset"
+					onClick={handleReset}
+				>
+					Сбросить
+				</button>
 			</div>
-			<h3 className="text-xl">Количество комнат</h3>
-			<div className="flex content-evenly flex-wrap">
-				{[1, 2, 3, 4].map((label, index) => (
-					<Checkbox
-						className={"mr-4"}
-						key={index}
-						label={String(label)}
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div className="font-bold text-base">Цена</div>
+				<div className="flex flex-wrap">
+					<Input
+						type="text"
+						placeholder="От"
+						{...register("priceFrom")}
 					/>
-				))}
-			</div>
-			<h3 className="text-xl">Дополнительно</h3>
-			<Checkbox label={"Без залога"} />
-			<Checkbox label={"Без предоплаты"} />
-			<Checkbox label={"Балкон"} />
-			<Checkbox label={"С животными"} />
-			<div className="flex content-evenly flex-wrap">
-				<Switch checked disabled label={"Жилье от собственника"} />
-			</div>
-			<Button>Применить фильтры</Button>
-		</>
+					<Input
+						type="text"
+						placeholder="До"
+						{...register("priceTo")}
+					/>
+				</div>
+				<div className="font-bold text-base">Количество комнат</div>
+				<div className="flex flex-wrap content-evenly">
+					{[1, 2, 3, 4].map((index) => (
+						<Checkbox
+							className="mr-4"
+							key={index}
+							label={String(index)}
+							name={`rooms[${index}]`}
+							register={register}
+						/>
+					))}
+				</div>
+				<div className="font-bold text-base">Дополниaтельно</div>
+				<Checkbox
+					label={"Без залога"}
+					name="additional.noDeposit"
+					register={register}
+				/>
+				<Checkbox
+					label={"Без предоплаты"}
+					name="additional.noPrepayment"
+					register={register}
+				/>
+				<Checkbox
+					label={"Балкон"}
+					name="additional.balcony"
+					register={register}
+				/>
+				<Checkbox
+					label={"С животными"}
+					name="additional.pets"
+					register={register}
+				/>
+				<Button type="submit">Применить фильтры</Button>
+			</form>
+		</div>
 	);
 }
 
