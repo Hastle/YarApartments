06452ed3c59bb4fefa48ec332c5f32c2df9d980b
Hastle <alexandrs.me.com@gmail.com>
2024-05-09 21:55:07 +0300

    change import for useApartmentDetails hook

--- src/pages/ApartmentDetails.js
+++ src/pages/ApartmentDetails.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useApartmentDetails } from "../hooks/useApartmentDetails";
+import useApartmentDetails from "../hooks/useApartmentDetails";
 
 function ApartmentDetails() {
 	let { id } = useParams();
