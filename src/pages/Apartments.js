69ce8a26cf63edf36856026b5172f4fe2784fa04
Hastle <alexandrs.me.com@gmail.com>
2024-05-09 07:42:27 +0300

    change styles

--- src/pages/Apartments.js
+++ src/pages/Apartments.js
@@ -1,27 +1,36 @@
 import React from "react";
 import Filter from "../components/Filter/Filter";
 import Card from "../components/Card/Card";
-
+import useApartments from "../hooks/useApartments";
 import data from "../data/DataApartments";
 
 function Apartments() {
+	// const { apartments, isLoading, error } = useApartments();
+	//
+	// if (isLoading) return <div>Loading...</div>;
+	// if (error) return <div>An error has occurred: {error.message}</div>;
+	//
+
 	return (
 		<>
-			<div className="lg:w-1/4 w-full">
+			<div className="w-full lg:w-1/4">
 				<Filter />
 			</div>
-			<div className="lg:w-3/4 md:w-full">
-				<h1 className="text-xl">Результаты поиска</h1>
-				<div className="flex flex-wrap">
-					{data.map((apartment) => (
-						<div key={apartment.id} className="w-full md:w-1/3 p-3">
-							<Card
-								address={apartment.address}
-								price={apartment.price}
-								imageUrl={apartment.imageUrl}
-							/>
-						</div>
-					))}
+			<div className="md:w-full lg:w-3/4">
+				<div className="p-3">
+					<h1 className="text-xl">Результаты поиска</h1>
+					<div className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
+						{data.map((apartment) => (
+							<div key={apartment.id}>
+								<Card
+									id={apartment.id}
+									address={apartment.address}
+									price={apartment.price}
+									imageUrl={apartment.imageUrl}
+								/>
+							</div>
+						))}
+					</div>
 				</div>
 			</div>
 		</>
