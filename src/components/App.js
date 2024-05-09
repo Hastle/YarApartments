1a2d63ce47d55a35f3cc1cd219d9847d44f70745
Hastle <alexandrs.me.com@gmail.com>
2024-05-09 07:41:20 +0300

    update routes, add QueryClientProvider

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import MainPage from "../pages/MainPage";
 import Apartments from "../pages/Apartments";
+import ApartmentDetails from "../pages/ApartmentDetails";
 import About from "../pages/About";
 import Contacts from "../pages/Contacts";
 import Login from "../pages/Login";
@@ -11,20 +13,28 @@ import Profile from "../pages/Profile";
 import ScrollToTop from "../utils/ScrollToTop";
 
 function App() {
+	const queryClient = new QueryClient();
+
 	return (
-		<BrowserRouter>
-			<ScrollToTop />
-			<Routes>
-				<Route path="/" element={<MainPage />} />
-				<Route element={<Layout />}>
-					<Route path="/apartments" element={<Apartments />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/contacts" element={<Contacts />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/profile" element={<Profile />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<ScrollToTop />
+				<Routes>
+					<Route path="/" element={<MainPage />} />
+					<Route element={<Layout />}>
+						<Route path="/apartments" element={<Apartments />} />
+						<Route
+							path="/apartments/:id"
+							element={<ApartmentDetails />}
+						/>
+						<Route path="/about" element={<About />} />
+						<Route path="/contacts" element={<Contacts />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/profile" element={<Profile />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
 	);
 }
 
