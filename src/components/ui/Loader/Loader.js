05f7173ab7014cf8a458b6184fb452bb7fdaefbf
Hastle <alexandrs.me.com@gmail.com>
2024-05-10 07:26:11 +0300

    change styles

--- src/components/ui/Loader/Loader.js
+++ src/components/ui/Loader/Loader.js
@@ -3,7 +3,7 @@ import { BiLoaderAlt } from "react-icons/bi";
 
 function Loader() {
 	return (
-		<div className="relative flex h-full w-full items-center justify-center">
+		<div className="flex h-full w-full items-center justify-center">
 			<BiLoaderAlt className="animate-spin text-green" size="50" />
 		</div>
 	);
