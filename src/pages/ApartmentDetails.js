e779fb73b6c15d6c0669952b621ac995f02f4313
Hastle <alexandrs.me.com@gmail.com>
2024-05-10 05:49:36 +0300

    use Loader component, change error handlers

--- src/pages/ApartmentDetails.js
+++ src/pages/ApartmentDetails.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import useApartmentDetails from "../hooks/useApartmentDetails";
+import Loader from "../components/ui/Loader/Loader";
 
 function ApartmentDetails() {
 	let { id } = useParams();
 	// const { data: apartment, isLoading, error } = useApartmentDetails(id);
 	//
-	// if (isLoading) return <div>Loading...</div>;
-	// if (error) return <div>An error has occurred: {error.message}</div>;
-	// if (!apartment) return <div>No data available</div>;
+	// if (isLoading) return <Loader />;
+	// if (error) return <div>Произошла ошибка: {error.message}</div>;
+	// if (!apartment) return <div>Данные не доступны</div>;
 
 	return (
 		<div>
 			<h2>Apartment Details Page</h2>
-			<p className="text-bold rounded-lg border p-5 shadow-lg">
+			<div className="text-bold rounded-lg border p-5 shadow-lg">
 				Object ID: {id}
-			</p>
+			</div>
 			{/*<h3>{apartment.title}</h3>*/}
 			{/*<p>Price: {apartment.price}</p>*/}
 			{/*<p>Address: {apartment.address}</p>*/}
