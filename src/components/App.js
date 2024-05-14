578591cae1e4e9f3cd6dc35e0f3ee513df37d26f
Hastle <alexandrs.me.com@gmail.com>
2024-05-14 11:40:25 +0300

    add YMaps provider

--- src/components/App.js
+++ src/components/App.js
@@ -11,29 +11,35 @@ import Login from "../pages/Login";
 import Profile from "../pages/Profile";
 
 import ScrollToTop from "../utils/ScrollToTop";
+import { YMaps } from "@pbe/react-yandex-maps";
 
 function App() {
 	const queryClient = new QueryClient();
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
-				<ScrollToTop />
-				<Routes>
-					<Route path="/" element={<MainPage />} />
-					<Route element={<Layout />}>
-						<Route path="/apartments" element={<Apartments />} />
-						<Route
-							path="/apartments/:id"
-							element={<ApartmentDetails />}
-						/>
-						<Route path="/about" element={<About />} />
-						<Route path="/contacts" element={<Contacts />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/profile" element={<Profile />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
+			<YMaps query={{ lang: "en_RU" }}>
+				<BrowserRouter>
+					<ScrollToTop />
+					<Routes>
+						<Route path="/" element={<MainPage />} />
+						<Route element={<Layout />}>
+							<Route
+								path="/apartments"
+								element={<Apartments />}
+							/>
+							<Route
+								path="/apartments/:id"
+								element={<ApartmentDetails />}
+							/>
+							<Route path="/about" element={<About />} />
+							<Route path="/contacts" element={<Contacts />} />
+							<Route path="/login" element={<Login />} />
+							<Route path="/profile" element={<Profile />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</YMaps>
 		</QueryClientProvider>
 	);
 }
