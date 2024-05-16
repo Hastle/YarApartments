351ee0f3449bc706360560c2d876d123d8579bfd
Hastle <alexandrs.me.com@gmail.com>
2024-05-16 12:34:23 +0300

    change name on MultiPlacemarkMap component

---
+++ src/components/MultiPlacemarkMap/MultiPlacemarkMap.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Map, Placemark } from "@pbe/react-yandex-maps";
+import markerIcon from "../../assets/images/marker-icon.svg";
+
+const MultiPlacemarkMap = ({
+	className = "",
+	center,
+	zoom,
+	controls,
+	modules,
+	locations,
+}) => {
+	const renderPlacemarks = () => {
+		return locations.map((location, index) => (
+			<Placemark
+				key={index}
+				modules={["geoObject.addon.balloon"]}
+				geometry={location.coordinates}
+				properties={{
+					hintContent: location.hint,
+					balloonContent: location.balloon,
+				}}
+				options={{
+					iconLayout: "default#image",
+					iconImageHref: markerIcon,
+					iconImageSize: [30, 42],
+					iconImageOffset: [-15, -42],
+				}}
+			/>
+		));
+	};
+
+	return (
+		<Map
+			className={className}
+			defaultState={{
+				center,
+				zoom,
+				controls,
+			}}
+			modules={modules}
+		>
+			{renderPlacemarks()}
+		</Map>
+	);
+};
+
+export default MultiPlacemarkMap;
