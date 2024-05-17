af59aa0cfbe45980b21c7c1d60d83b96cda11ef0
Hastle <alexandrs.me.com@gmail.com>
2024-05-17 13:32:50 +0300

    remove unused imports

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,4 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import Input from "../components/ui/Input/Input";
-import Checkbox from "../components/ui/Checkbox/Checkbox";
-import Button from "../components/ui/Button/Button";
 import AuthForm from "../components/AuthForm/AuthForm";
 function Login() {
 	return (
