88721d2a89142fd853971a46c8d16b3ebe14856f
Hastle <alexandrs.me.com@gmail.com>
2024-05-17 13:29:52 +0300

    use AuthForm component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
+import Input from "../components/ui/Input/Input";
+import Checkbox from "../components/ui/Checkbox/Checkbox";
+import Button from "../components/ui/Button/Button";
+import AuthForm from "../components/AuthForm/AuthForm";
 function Login() {
-	return <div>Login</div>;
+	return (
+		<div className="flex w-full items-center justify-center">
+			<AuthForm />
+		</div>
+	);
 }
 
 export default Login;
