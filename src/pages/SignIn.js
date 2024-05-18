a327da3a603acc927743d12edca6e33995710f20
Hastle <alexandrs.me.com@gmail.com>
2024-05-18 08:32:31 +0300

    remove unused imports

---
+++ src/pages/SignIn.js
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginForm from "../components/Auth/LoginForm";
+function SignIn() {
+	return (
+		<div className="flex w-full items-center justify-center">
+			<LoginForm />
+		</div>
+	);
+}
+
+export default SignIn;
