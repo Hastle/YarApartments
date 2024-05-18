23319a6e797b0aba2cb51c45745bc5112b4af3ce
Hastle <alexandrs.me.com@gmail.com>
2024-05-18 08:26:33 +0300

    use AuthProvider, PrivateRoute, add new routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { AuthProvider } from "../contexts/AuthContext";
+import PrivateRoute from "../components/PrivateRoute/PrivateRoute";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout/Layout";
@@ -7,7 +9,8 @@ import Apartments from "../pages/Apartments";
 import ApartmentDetails from "../pages/ApartmentDetails";
 import About from "../pages/About";
 import Contacts from "../pages/Contacts";
-import Login from "../pages/Login";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 import Profile from "../pages/Profile";
 
 import ScrollToTop from "../utils/ScrollToTop";
@@ -17,30 +20,43 @@ function App() {
 	const queryClient = new QueryClient();
 
 	return (
-		<QueryClientProvider client={queryClient}>
-			<YMaps query={{ lang: "en_RU" }}>
-				<BrowserRouter>
-					<ScrollToTop />
-					<Routes>
-						<Route path="/" element={<MainPage />} />
-						<Route element={<Layout />}>
-							<Route
-								path="/apartments"
-								element={<Apartments />}
-							/>
-							<Route
-								path="/apartments/:id"
-								element={<ApartmentDetails />}
-							/>
-							<Route path="/about" element={<About />} />
-							<Route path="/contacts" element={<Contacts />} />
-							<Route path="/login" element={<Login />} />
-							<Route path="/profile" element={<Profile />} />
-						</Route>
-					</Routes>
-				</BrowserRouter>
-			</YMaps>
-		</QueryClientProvider>
+		<AuthProvider>
+			<QueryClientProvider client={queryClient}>
+				<YMaps query={{ lang: "en_RU" }}>
+					<BrowserRouter>
+						<ScrollToTop />
+						<Routes>
+							<Route path="/" element={<MainPage />} />
+							<Route element={<Layout />}>
+								<Route
+									path="/apartments"
+									element={<Apartments />}
+								/>
+								<Route
+									path="/apartments/:id"
+									element={<ApartmentDetails />}
+								/>
+								<Route path="/about" element={<About />} />
+								<Route
+									path="/contacts"
+									element={<Contacts />}
+								/>
+								<Route path="/signin" element={<SignIn />} />
+								<Route path="/signup" element={<SignUp />} />
+								<Route
+									path="/profile"
+									element={
+										<PrivateRoute>
+											<Profile />
+										</PrivateRoute>
+									}
+								/>
+							</Route>
+						</Routes>
+					</BrowserRouter>
+				</YMaps>
+			</QueryClientProvider>
+		</AuthProvider>
 	);
 }
 
