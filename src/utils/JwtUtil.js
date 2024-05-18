8f76a33c2ecb0107bc5359946e5c4b0fdc38807f
Hastle <alexandrs.me.com@gmail.com>
2024-05-18 14:33:40 +0300

    remove JWT_KEY

--- src/utils/JwtUtil.js
+++ src/utils/JwtUtil.js
@@ -1,13 +1,10 @@
 import * as jose from "jose";
 
-const JWT_KEY = process.env.JWT_KEY;
-
 export const decodeToken = (token) => {
 	if (!token) {
 		console.log("No token provided");
 		return null;
 	}
-
 	try {
 		return jose.decodeJwt(token);
 	} catch (error) {
