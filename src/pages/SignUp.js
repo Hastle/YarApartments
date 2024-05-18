1bc602b2f5b789ddff30ee098be6978e1a6e1063
Hastle <alexandrs.me.com@gmail.com>
2024-05-18 08:29:00 +0300

    create SignUp page

---
+++ src/pages/SignUp.js
@@ -0,0 +1,11 @@
+import React from "react";
+import RegisterForm from "../components/Auth/RegisterForm";
+function SignIn() {
+	return (
+		<div className="flex w-full items-center justify-center">
+			<RegisterForm />
+		</div>
+	);
+}
+
+export default SignIn;
