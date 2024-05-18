d35064a30811f2023d403bf39929077f805cd0b2
Hastle <alexandrs.me.com@gmail.com>
2024-05-18 08:28:12 +0300

    create PrivateRoute component

---
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+
+const PrivateRoute = ({ children }) => {
+	const { user } = useContext(AuthContext);
+
+	return user ? children : <Navigate to="/signin" />;
+};
+
+export default PrivateRoute;
