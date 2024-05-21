09d9cce18918c559d1ff8df3c79754ec9300e6a5
Hastle <alexandrs.me.com@gmail.com>
2024-05-21 17:42:31 +0300

    change function to const

--- src/components/ui/Loader/Loader.js
+++ src/components/ui/Loader/Loader.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BiLoaderAlt } from "react-icons/bi";
 
-function Loader() {
+const Loader = () => {
 	return (
 		<div className="flex h-full w-full items-center justify-center">
 			<BiLoaderAlt className="animate-spin text-green" size="50" />
 		</div>
 	);
-}
+};
 
 export default Loader;
