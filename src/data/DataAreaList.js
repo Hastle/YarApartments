61b7b998e2af402a62ff9d930c1488d32248dfde
Hastle <alexandrs.me.com@gmail.com>
2024-05-21 13:23:12 +0300

    create DataAreaList, DataSocialList

---
+++ src/data/DataAreaList.js
@@ -0,0 +1,9 @@
+const DataAreaList = [
+	{ name: "Кировский" },
+	{ name: "Ленинский" },
+	{ name: "Фрунзенский" },
+	{ name: "Дзержинский" },
+	{ name: "Красноперекопский" },
+	{ name: "Заволжский" },
+];
+export default DataAreaList;
---
+++ src/data/DataSocialList.js
@@ -0,0 +1,9 @@
+import { FaVk, FaFacebookF, FaWhatsapp } from "react-icons/fa";
+
+const DataSocialList = [
+	{ name: "vk", to: "https://vk.com", icon: FaVk },
+	{ name: "facebook", to: "https://facebook.com", icon: FaFacebookF },
+	{ name: "whatsup", to: "https://whatsup.com", icon: FaWhatsapp },
+];
+
+export default DataSocialList;
