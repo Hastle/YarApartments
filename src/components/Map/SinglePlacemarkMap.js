5eafbc15440871031c2acf756f64f4a9702f41d3
Hastle <alexandrs.me.com@gmail.com>
2024-05-24 22:14:35 +0300

    create SinglePlacemarkMap component

---
+++ src/components/Map/SinglePlacemarkMap.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Map, Placemark } from "@pbe/react-yandex-maps";
+import markerIcon from "../../assets/images/marker-icon.svg";
+
+const SinglePlacemarkMap = ({
+	className = "",
+	center,
+	zoom,
+	controls,
+	modules,
+	location,
+}) => {
+	const renderPlacemark = () => {
+		return (
+			<Placemark
+				modules={["geoObject.addon.balloon"]}
+				geometry={location.coordinates}
+				properties={{
+					hintContent: location.hint,
+					balloonContent: location.balloon,
+				}}
+				options={{
+					iconLayout: "default#image",
+					iconImageHref: markerIcon,
+					iconImageSize: [30, 42],
+					iconImageOffset: [-15, -42],
+					lang: "ru_RU",
+				}}
+			/>
+		);
+	};
+
+	return (
+		<Map
+			className={className}
+			defaultState={{
+				center,
+				zoom,
+				controls,
+			}}
+			modules={modules}
+		>
+			{renderPlacemark()}
+		</Map>
+	);
+};
+
+export default SinglePlacemarkMap;
